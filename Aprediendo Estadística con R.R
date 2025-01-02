edd44870c6cc16f97ff6ae7ab195fc948b11d10a
Agustín Gagliardi <agustin.gagliardi.castro@gmail.com>
2025-01-02 16:24:38 -0300

    abrir y ver base de datos

--- Aprediendo Estadística con R.R
+++ Aprediendo Estadística con R.R
@@ -1,1 +1,10 @@
 #Aprendiendo Estadística con R Studio
+
+
+data <- read.csv("global air pollution dataset.csv" , header = T , sep= ",")
+
+#Comando para trabajar con  los encabezados
+attach(data)
+
+#Mostrando las primeras filas de nuestros datos
+head(data)
